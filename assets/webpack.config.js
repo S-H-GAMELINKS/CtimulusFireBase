83903f721011d870abf53a37e4a656dd38839687
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-17 13:20:48 +0900

    using dotenv-webpack

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,7 +1,12 @@
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'index.js',
         path: `${__dirname}`
-    }
+    },
+    plugins: [
+        new Dotenv()
+    ]
 }
