98ca0aeba8921582dd6dc45252f298eff91fede6
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-17 13:53:18 +0900

    using firebase & add submit()

--- assets/src/controllers/post_controller.js
+++ assets/src/controllers/post_controller.js
@@ -1,4 +1,16 @@
 import { Controller } from "stimulus"
+import FireBase from 'firebase'
+
+const firebase = FireBase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGEING_SENDER_ID
+});
+
+const database = firebase.database();
 
 export default class extends Controller {
     static get targets() {
@@ -7,5 +19,12 @@ export default class extends Controller {
 
     content() {
         this.previewTarget.innerHTML = this.contentTarget.value
+    }
+
+    submit() {
+        database.ref('ctimuluse').push({
+            content: this.contentTarget.value
+        });
+        this.contentTarget.value = "";
     }
 }
